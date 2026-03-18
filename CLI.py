2b7ba69789d536f9a487dc107ffecf7a8ba88c1c
Angel251603 <167395858+Angel251603@users.noreply.github.com>
2026-03-17 22:30:20 -0500

    Add files via upload

---
+++ CLI.py
@@ -0,0 +1,20 @@
+from BMI import calculate_bmi
+
+# def get_height_in_inches():
+#     feet = int(input("Enter height (feet): "))
+#     inches = int(input("Enter height (inches): "))
+#     return (feet * 12) + inches
+
+# def main():
+#     print("=== BMI Calculator ===")
+#     height = get_height_in_inches()
+#     weight = float(input("Enter weight (pounds): "))
+#     bmi, category = calculate_bmi(height, weight)
+#     print(f"\nYour BMI: {bmi}")
+#     print(f"Category: {category}")
+
+# if __name__ == "__main__":
+#     main()
+for w in range(100, 300):
+    bmi, group = calculate_bmi(w, 65)
+    print(f"weight={w}, bmi={bmi}, group={group}")
